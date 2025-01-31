c4161b4759be14f8fee5a00c2b1757c22b93c22d
Anu <77046926+anupriyasingh317@users.noreply.github.com>
2025-01-31 16:56:28 +0530

    Update validate_account_status.sql

--- PLSQL/CARDS/validate_account_status.sql
+++ PLSQL/CARDS/validate_account_status.sql
@@ -4,10 +4,10 @@ CREATE OR REPLACE FUNCTION validate_account_status(p_account_id IN NUMBER) RETUR
 BEGIN
     -- Simulate an account status check from the accounts table
     SELECT account_status INTO v_account_status FROM accounts WHERE account_id = p_account_id;
-
     IF v_account_status = 'ACTIVE' THEN
         RETURN TRUE;
     ELSE
+        transaction_pkg.record_transaction(1, 'Credit', 1000, 10, 'Success');
         RETURN FALSE;
     END IF;
 END validate_account_status;
